686e97b8f94bc815cb20f3e3ed5d27a4f12c607b
zpfz <zpfz@seeyoz.cn>
2021-05-16 23:15:18 +0800

    2021/05/16

--- docs/.vuepress/components/Msg.vue
+++ docs/.vuepress/components/Msg.vue
@@ -3,12 +3,12 @@
 export default {
   name: "Msg",
   mounted() {
-    let readkey = localStorage.getItem('20210512');
+    let readkey = localStorage.getItem('20210516');
     if (readkey == false || readkey == null) {
-      localStorage.setItem('20210512', 'true');
+      localStorage.setItem('20210516', 'true');
       this.$notification.open({
         message: "AntDocs官网地址更新通知",
-        description: "原文档地址 antdocs.seeyoz.cn 即将弃用，目前已启用 antdocs.vercel.app 域名作为 AntDocs 官网地址，请大家及时保存最新官网链接哦！",
+        description: "由于近期 Vercel 服务出现波动，计划将 antdocs.pages.dev 作为文档主题主域名，而 antdocs.vercel.app 作为备用域名，给大家带来不便敬请谅解！",
         icon: <a-icon type="exclamation-circle" style="color: #1890ff" />,
         duration: 0,
         placement: "bottomRight",
@@ -27,7 +27,7 @@ export default {
         //     "查看更新"
         //   );
         // },
-        key: "20210512"
+        key: "20210516"
       });
     }
   }
