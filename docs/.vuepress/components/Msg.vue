1188a519456eb02d27a3bcd6001fbfc7c666b18f
Feng L.H <49757965+zpfz@users.noreply.github.com>
2022-08-28 01:59:21 +0800

    Update Msg.vue

--- docs/.vuepress/components/Msg.vue
+++ docs/.vuepress/components/Msg.vue
@@ -8,7 +8,7 @@ export default {
       localStorage.setItem('20220329', 'true');
       this.$notification.open({
         message: "AntDocs官网地址更新通知",
-        description: "由于近期 CloudFlare 服务出现波动，计划将 antdocs.vercel.app 作为文档主题主域名，而 antdocs.pages.dev 作为备用域名，给大家带来不便敬请谅解！",
+        description: "由于近期 Vercel/CloudFlare 服务出现波动，计划将 antdocs.netlify.app 作为文档主题主域名，其他作为备用域名，给大家带来不便敬请谅解！",
         icon: <a-icon type="exclamation-circle" style="color: #1890ff" />,
         duration: 0,
         placement: "bottomRight",
